f7b046741fe93578c59a2ccf33af875ef056b632
klaus-oehling <127336537+klaus-oehling@users.noreply.github.com>
2024-03-13 15:36:59 -0300

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 10:22:15 2024
+
+@author: oehli
+"""
+
+import streamlit as st
+
+st.set_page_config(page_title="será")
+
+st.title("Bem-vindo ao site do Insper Quantitative Finance Club")
+
+st.write("Aqui você encontrará diversos conteúdos relacionados ao mercado financeiro. Combinando programação e teorias básicas de finanças, proporcionamos programas interativos para visualização e aprendizado de conceitos amplamente usados no mercado de trabalho. A integração do mundo quantitativo e computacional à finanças corporativas nos move. Esperamos que nossos programas sejam de grande utilidade tanto para a comunidade Insper, quanto para qualquer um interessado nos temas. Obrigado!")
+
+st.divider()
